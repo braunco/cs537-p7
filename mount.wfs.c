12f46cd0e996a4b02f69f017872842fc09b4ca23
Connor Braun <cwbraun8@gmail.com>
2023-12-04 15:32:56 -0600

    skeletons for methods

--- mount.wfs.c
+++ mount.wfs.c
@@ -22,11 +22,55 @@ static int wfs_getattr(const char *path, struct stat *stbuf) {
     return 0;
 }
 
+static int wfs_mknod(const char *path, mode_t mode, dev_t rdev) {
+    // Check if the file already exists
+    // If not, create a new inode and log entry for the file
+    // Append the log entry to the disk
+    // Update the parent directory's log entry to include this new file
+    return 0; // or appropriate error code
+}
 
-// Implement other necessary operations...
+static int wfs_mkdir(const char *path, mode_t mode) {
+    // Similar to wfs_mknod, but for directories
+    // Remember to set the mode of the inode to indicate a directory (S_IFDIR)
+    return 0; // or appropriate error code
+}
+
+static int wfs_write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi) {
+    // Locate the file's inode using the path
+    // Append a new log entry with the updated file content
+    return size; // return the number of bytes written or an error code
+}
+
+static int wfs_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi) {
+    // Locate the file's inode and its log entry
+    // Read the content into buf
+    return size; // return the number of bytes read or an error code
+}
+
+static int wfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi) {
+    // Locate the directory's inode and its log entry
+    // For each entry in the directory, use filler() to add it to the list
+    return 0; // or appropriate error code
+}
+
+static int wfs_unlink(const char *path) {
+    // Locate the file's inode
+    // Mark the file's inode as deleted in a new log entry
+    // Update the parent directory's log entry to remove this file
+    return 0; // or appropriate error code
+}
+
+
 
 static struct fuse_operations wfs_oper = {
     .getattr = wfs_getattr,
+    .mknod = wfs_mknod,
+    .mkdir = wfs_mkdir,
+    .write = wfs_write,
+    .read = wfs_read,
+    .readdir = wfs_readdir,
+    .unlink = wfs_unlink,
     // Add other operations (read, write, mkdir, etc.)
 };
 
